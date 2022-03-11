17aa135cb771d6948bec2568d372728668bde194
Hyoban <hyoban@qq.com>
2022-03-11 21:11:00 +0800

    编写界面部分

--- src_crx/src_options/App.tsx
+++ src_crx/src_options/App.tsx
@@ -1,5 +1,28 @@
 function App() {
-  return <div>options</div>;
+  return (
+    <div className="bg-gray-200 min-h-screen pt-2">
+      <div className="container mx-auto inputs w-full max-w-2xl p-6 mx-auto">
+        <h2 className="text-2xl text-gray-900">账户设置</h2>
+        <div className="w-full md:w-full px-3 mb-6 mt-6 border-t border-gray-400 pt-4">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            token
+          </label>
+          <input
+            className="appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+            type="text"
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            className="appearance-none bg-gray-200 text-gray-900 px-2 py-1 shadow-sm border border-gray-400 rounded-md mr-3"
+            type="submit"
+          >
+            保存
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src_crx/src_popup/App.tsx
+++ src_crx/src_popup/App.tsx
@@ -1,5 +1,24 @@
 function App() {
-  return <div>popup</div>;
+  return (
+    <div className="w-96 h-96 p-3 flex flex-col bg-gray-200">
+      <label htmlFor="content" className="mb-3 inline-block text-lg">
+        笔记：
+      </label>
+      <textarea
+        id="content"
+        rows={10}
+        placeholder="请输入 markdown 格式笔记"
+        className="mb-3 appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+        required
+      />
+      <button
+        type="submit"
+        className="self-end mr-2 w-24 py-2 px-4 appearance-none bg-gray-200 text-gray-900 shadow-sm border border-gray-400 rounded-md"
+      >
+        发送
+      </button>
+    </div>
+  );
 }
 
 export default App;
